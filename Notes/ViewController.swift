7f6260ff747b5ae1036f986b3a026bfdae76bae8
VIS1337 <vasily.saliy@icloud.com>
2021-03-19 04:44:22 +0600

    note

---
+++ Notes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by VIS on 16.03.2021.
+//  Copyright © 2021 Vasily SALIY. All rights reserved.
+//
+
+import UIKit
+//notes.insert(["Swipe me left": "I am note"], at: 0)
+
+
+
+
+class ViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
